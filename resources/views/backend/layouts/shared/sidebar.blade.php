86ac49a4bb1e69bf07485c67592b7bcee763fd6f
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-04-18 11:41:36 +0200

    Starting to Design the Static website for sumburero

--- resources/views/backend/layouts/shared/sidebar.blade.php
+++ resources/views/backend/layouts/shared/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="{{ route('dashboard') }}">
+        <a class="sidebar-brand" href="{{ url('/') }}">
             <span class="align-middle">{{ config('app.name', 'laravel') }}</span>
         </a>
         <ul class="sidebar-nav">
