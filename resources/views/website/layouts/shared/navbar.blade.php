8bbb9f7d57f43f971085cc944a2b165b0e799d4a
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-05-07 14:37:19 +0200

    banner alert added

--- resources/views/website/layouts/shared/navbar.blade.php
+++ resources/views/website/layouts/shared/navbar.blade.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top sh">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Sumburero') }}</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
             aria-controls="offcanvasExample">
             <span class="navbar-toggler-icon"></span>
@@ -20,13 +21,13 @@
                 <li class="nav-item">
                     <a class="nav-link ">Contacte -nos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-danger rounded-3 px-5" style="padding: 4px;color:white">Website ainda em desenvolvimento</a>
-                </li>
             </ul>
         </div>
     </div>
 </nav>
+<div class="alert alert-danger sticky" role="alert">
+    <strong>O website Sumburero ainda esta em desenvolvimento</strong>
+</div>
 
 
 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
